46476b53643ee9c06c39b2be40ef4b17c4298817
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-10-20 14:16:43 +0100

    Add project files.

---
+++ FabAppLanche/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FabAppLanche
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
