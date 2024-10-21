ccee020964b14701da31d1e8feee783b18f123b7
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-10-21 14:53:42 +0100

    Displaying shopping cart items.

--- FabAppLanche/Pages/CarrinhoPage.xaml.cs
+++ FabAppLanche/Pages/CarrinhoPage.xaml.cs
@@ -1,9 +1,132 @@
+using FabAppLanche.Models;
+using FabAppLanche.Services;
+using FabAppLanche.Validations;
+using System.Collections.ObjectModel;
+
 namespace FabAppLanche.Pages;
 
 public partial class CarrinhoPage : ContentPage
 {
-	public CarrinhoPage()
-	{
-		InitializeComponent();
-	}
+    private readonly ApiService _apiService;
+    private readonly IValidator _validator;
+    private bool _loginPageDisplayed = false;
+    private bool _isNavigatingToEmptyCartPage = false;
+
+    private ObservableCollection<CarrinhoCompraItem>
+        ItensCarrinhoCompra = new ObservableCollection<CarrinhoCompraItem>();
+
+    public CarrinhoPage(ApiService apiService, IValidator validator)
+    {
+        InitializeComponent();
+        _apiService = apiService;
+        _validator = validator;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await GetItensCarrinhoCompra();
+    }
+
+    private async Task<IEnumerable<CarrinhoCompraItem>> GetItensCarrinhoCompra()
+    {
+        try
+        {
+            var usuarioId = Preferences.Get("usuarioid", 0);
+            var (itensCarrinhoCompra, errorMessage) = await
+                     _apiService.GetItensCarrinhoCompra(usuarioId);
+
+            if (errorMessage == "Unauthorized" && !_loginPageDisplayed)
+            {
+                // Redirecionar para a página de login
+                await DisplayLoginPage();
+                return Enumerable.Empty<CarrinhoCompraItem>();
+            }
+
+            if (itensCarrinhoCompra == null)
+            {
+                await DisplayAlert("Erro", errorMessage ?? "Não foi possivel obter os itens do carrinho de compra.", "OK");
+                return Enumerable.Empty<CarrinhoCompraItem>();
+            }
+
+            ItensCarrinhoCompra.Clear();
+            foreach (var item in itensCarrinhoCompra)
+            {
+                ItensCarrinhoCompra.Add(item);
+            }
+
+            CvCarrinho.ItemsSource = ItensCarrinhoCompra;
+            AtualizaPrecoTotal();
+            return ItensCarrinhoCompra;
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", $"Ocorreu um erro inesperado: {ex.Message}", "OK");
+            return Enumerable.Empty<CarrinhoCompraItem>();
+        }
+    }
+    private void AtualizaPrecoTotal()
+    {
+        try
+        {
+            var precoTotal = ItensCarrinhoCompra.Sum(item => item.Preco * item.Quantidade);
+            LblPrecoTotal.Text = precoTotal.ToString();
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Erro", $"Ocorreu um erro ao atualizar o preço total: {ex.Message}", "OK");
+        }
+    }
+    private async Task DisplayLoginPage()
+    {
+        _loginPageDisplayed = true;
+
+        await Navigation.PushAsync(new LoginPage(_apiService, _validator));
+    }
+
+    private async void BtnDecrementar_Clicked(object sender, EventArgs e)
+    {
+        {
+            if (sender is Button button && button.BindingContext is CarrinhoCompraItem itemCarrinho)
+            {
+                itemCarrinho.Quantidade++;
+                AtualizaPrecoTotal();
+                await _apiService.AtualizaQuantidadeItemCarrinho(itemCarrinho.ProdutoId, "aumentar");
+            }
+
+        }
+    }
+
+    private async void BtnIncrementar_Clicked(object sender, EventArgs e)
+    {
+        if (sender is Button button && button.BindingContext is CarrinhoCompraItem itemCarrinho)
+        {
+            itemCarrinho.Quantidade++;
+            AtualizaPrecoTotal();
+            await _apiService.AtualizaQuantidadeItemCarrinho(itemCarrinho.ProdutoId, "aumentar");
+        }
+    }
+
+    private async void BtnDeletar_Clicked(object sender, EventArgs e)
+    {
+
+        if (sender is ImageButton button && button.BindingContext is CarrinhoCompraItem itemCarrinho)
+        {
+            bool resposta = await DisplayAlert("Confirma  o",
+                          "Tem certeza que deseja excluir este item do carrinho?", "Sim", "N o");
+            if (resposta)
+            {
+                ItensCarrinhoCompra.Remove(itemCarrinho);
+                AtualizaPrecoTotal();
+                await _apiService.AtualizaQuantidadeItemCarrinho(itemCarrinho.ProdutoId, "deletar");
+            }
+        }
+
+    }
+
+    private void BtnEditaEndereco_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
