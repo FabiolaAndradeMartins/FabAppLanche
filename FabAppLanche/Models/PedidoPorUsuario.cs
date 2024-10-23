b8718eb62a580656a901b234fc7b3c47a23688e6
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-10-23 16:13:53 +0100

    Implementing customer profile page functionality by implementing retrieving customer orders and order details.

---
+++ FabAppLanche/Models/PedidoPorUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabAppLanche.Models
+{
+    public class PedidoPorUsuario
+    {
+        public int Id { get; set; }
+        public decimal PedidoTotal { get; set; }
+        public DateTime DataPedido { get; set; }
+    }
+
+}
