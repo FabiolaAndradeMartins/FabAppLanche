c9a90b458ece153fc2d7f9effc2a227033208288
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-10-26 14:58:41 +0100

    Optimize the code of the ApiEcommerce API and the .NET MAUI AppLanches project.

--- FabAppLanche/Pages/PedidosPage.xaml.cs
+++ FabAppLanche/Pages/PedidosPage.xaml.cs
@@ -27,7 +27,11 @@ public partial class PedidosPage : ContentPage
         {
             try
             {
-                var (pedidos, errorMessage) = await _apiService.GetPedidosPorUsuario(Preferences.Get("usuarioid", 0));
+                // Exibe o indicador de carregamento
+                loadPedidosIndicator.IsRunning = true;
+                loadPedidosIndicator.IsVisible = true;
+
+            var (pedidos, errorMessage) = await _apiService.GetPedidosPorUsuario(Preferences.Get("usuarioid", 0));
 
                 if (errorMessage == "Unauthorized" && !_loginPageDisplayed)
                 {
@@ -53,7 +57,14 @@ public partial class PedidosPage : ContentPage
             {
                 await DisplayAlert("Erro", "Ocorreu um erro ao obter os pedidos. Tente novamente mais tarde.", "OK");
             }
-        }
+
+            finally
+            {
+                // Esconde o indicador de carregamento
+                loadPedidosIndicator.IsRunning = false;
+                loadPedidosIndicator.IsVisible = false;
+            }
+    }
 
         private void CvPedidos_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
